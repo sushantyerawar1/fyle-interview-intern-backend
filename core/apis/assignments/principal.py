1b3e8df1eb843bdc71c46a414f8d84dafc06cd1f
Sushant <sushantdyerawar2002@gmail.com>
2024-01-22 03:07:31 +0530

    Wrote all 3 missings api's

--- core/apis/assignments/principal.py
+++ core/apis/assignments/principal.py
@@ -5,19 +5,28 @@ from core.apis.responses import APIResponse
 from core.models.assignments import Assignment
 from core.models.teachers import Teacher
 
-from .schema import AssignmentSchema, AssignmentGradeSchema
+from .schema import AssignmentSchema, AssignmentGradeSchema, Teacherchema
 principal_assignments_resources = Blueprint('principal_assignments_resources', __name__)
 
 
 @principal_assignments_resources.route('/assignments', methods=['GET'], strict_slashes=False)
 @decorators.authenticate_principal
-def list_assignments(p):
+def list_all_assignments(p):
     """Returns list of assignments"""
 
     principal_assignments = Assignment.query.filter(Assignment.state == 'SUBMITTED' or  Assignment.state == 'GRADED').all()
     principal_assignments_dump = AssignmentSchema().dump(principal_assignments, many=True)
     return APIResponse.respond(data=principal_assignments_dump)
 
+
+@principal_assignments_resources.route('/teachers', methods=['GET'], strict_slashes=False)
+@decorators.authenticate_principal
+def list_all_teachers(p):
+    """Returns list of teachers"""
+
+    principal_teachers = Teacher.query.all()
+    principal_teachers_dump = Teacherchema().dump(principal_teachers, many=True)
+    return APIResponse.respond(data=principal_teachers_dump)
 
 
 @principal_assignments_resources.route('/assignments/grade', methods=['POST'], strict_slashes=False)
--- core/models/teachers.py
+++ core/models/teachers.py
@@ -11,3 +11,4 @@ class Teacher(db.Model):
 
     def __repr__(self):
         return '<Teacher %r>' % self.id
+
